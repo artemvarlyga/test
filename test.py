6e94245777be2d9ff114ff3ce36c5b716f054241
dda <sralnya@gmail.com>
2018-08-04 21:15:24 +0300

    added argparse part

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import json
 import boto3
 from botocore.exceptions import ClientError
 import paramiko
+import argparse
 
 my_region = "us-east-1"
 av_zone = my_region + "a"
@@ -39,184 +40,191 @@ def wait_for_ssh_to_be_ready (timeout, retry_interval):
             print('SSH is not ready...')
             continue
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--run_httpd", action="store_true", help="run the httpd server", dest="r")
+if parser.parse_args().r:
+        print ("starting httpd server...")
+elif parser.parse_args().r == False:
+        ec2 = boto3.resource('ec2', region_name = my_region)
+        ec2_client = boto3.client('ec2',region_name = my_region)
 
-ec2 = boto3.resource('ec2', region_name = my_region)
-ec2_client = boto3.client('ec2',region_name = my_region)
 
+        ### create security group ###
+        try:
+            sec_group = ec2.create_security_group(
+                GroupName=security_group_name,
+                Description='web sec group',
+                VpcId=vpc_id
+            )
 
-### create security group ###
-try:
-    sec_group = ec2.create_security_group(
-        GroupName=security_group_name,
-        Description='web sec group',
-        VpcId=vpc_id
-    )
+            sec_group.authorize_ingress(
+               CidrIp='0.0.0.0/0',
+               IpProtocol='tcp',
+               FromPort=80,
+               ToPort=80
+            )
+            sec_group.authorize_ingress(
+               CidrIp='0.0.0.0/0',
+               IpProtocol='tcp',
+               FromPort=22,
+               ToPort=22
+            )
+        except ClientError as e:
+           if e.response['Error']['Code'] == 'InvalidGroup.Duplicate':
+              pass
 
-    sec_group.authorize_ingress(
-       CidrIp='0.0.0.0/0',
-       IpProtocol='tcp',
-       FromPort=80,
-       ToPort=80
-    )
-    sec_group.authorize_ingress(
-       CidrIp='0.0.0.0/0',
-       IpProtocol='tcp',
-       FromPort=22,
-       ToPort=22
-    )
-except ClientError as e:
-   if e.response['Error']['Code'] == 'InvalidGroup.Duplicate':
-      pass
+        ### create volume ###
+        response = ec2_client.describe_volumes(
+            Filters=[
+                {
+                    'Name': 'tag:Name',
+                    'Values': [
+                        'web_vol'
+                    ]
+                },
+                {
+                    'Name': 'status',
+                    'Values': [
+                        'available'
+                    ]
+                },
+                {
+                    'Name': 'volume-type',
+                    'Values': [
+                        'standard'
+                    ]
+                },
+           ]
+        )
+        try:
+          volume_id  =  response['Volumes'][0]['VolumeId']
+        except IndexError:
+          volume_id = "0"
+          pass
 
-### create volume ###
-response = ec2_client.describe_volumes(
-    Filters=[
-        {
-            'Name': 'tag:Name',
-            'Values': [
-                'web_vol'
-            ]
-        },
-        {
-            'Name': 'status',
-            'Values': [
-                'available'
-            ]
-        },
-        {
-            'Name': 'volume-type',
-            'Values': [
-                'standard'
-            ]
-        },
-   ]
-)
-try:
-  volume_id  =  response['Volumes'][0]['VolumeId']
-except IndexError:
-  volume_id = "0"
-  pass
+        if volume_id == "0":
 
-if volume_id == "0":
-
-  volume = ec2.create_volume(
-     Size=1,
-     VolumeType='standard',
-     AvailabilityZone=av_zone,
-     TagSpecifications=[
-          {
-             'ResourceType': 'volume',
-             'Tags': [
-                 {
-                     'Key': 'Name',
-                     'Value': 'web_vol'
-                 }
-              ]
-         }
-     ]
-  )
-  volume_id = volume.id
-print("ebs volume %s is ready for your instance" % volume_id)
-
-### check if instance is already running ###
-response = ec2_client.describe_instances(
-    Filters=[
-        {
-            'Name': 'tag:Name',
-            'Values': [
-                'web_srv'
-            ]
-        },
-        {
-            'Name': 'instance-type',
-            'Values': [
-                instance_type
-            ]
-        },
-        {
-            'Name': 'instance-state-name',
-            'Values': [
-                'running'
-            ]
-        }
-    ]
-)
-try:
-  instance_id  =  response['Reservations'][0]['Instances'][0]['InstanceId']
-except IndexError:
-  instance_id = "0"
-  pass
-
-if instance_id == "0": ### then instance is absent and it has to be created
-
-  response = ec2.create_instances(ImageId=image_id,
-                       InstanceType=instance_type,
-                       MinCount=1, MaxCount=1,
-                       Placement=
+          volume = ec2.create_volume(
+             Size=1,
+             VolumeType='standard',
+             AvailabilityZone=av_zone,
+             TagSpecifications=[
+                  {
+                     'ResourceType': 'volume',
+                     'Tags': [
                          {
-                          'AvailabilityZone': av_zone
-                         },
-                       SecurityGroups=['web sg'],
-                       KeyName=key_pair_name,
-                       )
-  instance_id = response[0].instance_id
-  instance = ec2.Instance(instance_id)
-  instance.wait_until_running()
-  ec2_client.attach_volume(
-      VolumeId=volume_id,
-      InstanceId=instance_id,
-      Device='/dev/xvdl'
-  )
-### assign tags to instance ###
-  ec2.create_tags(
-      Resources=[
-        instance_id
-      ],
-      Tags=[
-        {
-          'Key': 'Name',
-          'Value': 'web_srv'
-        }
-      ]
-  )
-  print("%s has been created" % instance_id)
-###  connect to , format the disk, mount it and perform git installation and repo clone ###
-  wait_for_ssh_to_be_ready('20', '3')
-  print ("connected")
-  commands = [ "echo '/dev/xvdl /my_volume    ext4 defaults 0  2' | sudo tee /etc/fstab",
-               "sudo mkfs.ext4 /dev/xvdl",
-               "sudo mkdir /my_volume",
-               "sudo mount -a",
-               "sudo yum update -y",
-               "sudo yum -y install git",
-               "sudo mkdir /my_volume/my_git",
-               "sudo git clone https://github.com/artemvarlyga/test.git /my_volume/my_git",
+                             'Key': 'Name',
+                             'Value': 'web_vol'
+                         }
+                      ]
+                 }
              ]
-  for command in commands:
-  	print "Executing {}".format( command )
-  	stdin , stdout, stderr = c.exec_command(command)
-  	print stdout.read()
-  	print( "Errors")
-  	print stderr.read()
-  c.close()
-else:
-  print("Instance web_srv seems is alredy running in your AWS account")
+          )
+          volume_id = volume.id
+        print("ebs volume %s is ready for your instance" % volume_id)
 
-### retrieve dns and public IP from web_srv instanse ###
-response = ec2_client.describe_instances(
-    Filters=[
-        {
-            'Name': 'instance-id',
-            'Values': [
-                instance_id
+        ### check if instance is already running ###
+        response = ec2_client.describe_instances(
+            Filters=[
+                {
+                    'Name': 'tag:Name',
+                    'Values': [
+                        'web_srv'
+                    ]
+                },
+                {
+                    'Name': 'instance-type',
+                    'Values': [
+                        instance_type
+                    ]
+                },
+                {
+                    'Name': 'instance-state-name',
+                    'Values': [
+                        'running'
+                    ]
+                }
             ]
-        }
-    ]
-)
+        )
+        try:
+          instance_id  =  response['Reservations'][0]['Instances'][0]['InstanceId']
+        except IndexError:
+          instance_id = "0"
+          pass
 
-dns = response['Reservations'][0]['Instances'][0]['PublicDnsName']
-public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+        if instance_id == "0": ### then instance is absent and it has to be created
 
-### get a list of running instances with additional options ###
-for instance in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]):
-   print(instance.id, instance.instance_type, instance.key_name, instance.private_ip_address, instance.public_ip_address)
+          response = ec2.create_instances(ImageId=image_id,
+                               InstanceType=instance_type,
+                               MinCount=1, MaxCount=1,
+                               Placement=
+                                 {
+                                  'AvailabilityZone': av_zone
+                                 },
+                               SecurityGroups=['web sg'],
+                               KeyName=key_pair_name,
+                               )
+          instance_id = response[0].instance_id
+          instance = ec2.Instance(instance_id)
+          instance.wait_until_running()
+          ec2_client.attach_volume(
+              VolumeId=volume_id,
+              InstanceId=instance_id,
+              Device='/dev/xvdl'
+          )
+        ### assign tags to instance ###
+          ec2.create_tags(
+              Resources=[
+                instance_id
+              ],
+              Tags=[
+                {
+                  'Key': 'Name',
+                  'Value': 'web_srv'
+                }
+              ]
+          )
+          print("%s has been created" % instance_id)
+        ###  connect to , format the disk, mount it and perform git installation and repo clone ###
+          wait_for_ssh_to_be_ready('20', '3')
+          print ("connected")
+          commands = [ "echo '/dev/xvdl /my_volume    ext4 defaults 0  2' | sudo tee /etc/fstab",
+                       "sudo mkfs.ext4 /dev/xvdl",
+                       "sudo mkdir /my_volume",
+                       "sudo mount -a",
+                       "sudo yum update -y",
+                       "sudo yum -y install git",
+                       "sudo mkdir /my_volume/my_git",
+                       "sudo git clone https://github.com/artemvarlyga/test.git /my_volume/my_git",
+                     ]
+          for command in commands:
+          	print "Executing {}".format( command )
+          	stdin , stdout, stderr = c.exec_command(command)
+          	print stdout.read()
+          	print( "Errors")
+          	print stderr.read()
+          c.close()
+        else:
+          print("Instance web_srv seems is alredy running in your AWS account")
+
+        ### retrieve dns and public IP from web_srv instanse ###
+        response = ec2_client.describe_instances(
+            Filters=[
+                {
+                    'Name': 'instance-id',
+                    'Values': [
+                        instance_id
+                    ]
+                }
+            ]
+        )
+
+        dns = response['Reservations'][0]['Instances'][0]['PublicDnsName']
+        public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+
+        ### get a list of running instances with additional options ###
+        for instance in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]):
+           print(instance.id, instance.instance_type, instance.key_name, instance.private_ip_address, instance.public_ip_address)
+else:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
