df74642a69da6e8020a9bf831d8231f7a8e3d322
dda <sralnya@gmail.com>
2018-08-05 18:31:09 +0300

    changed directory

--- sss.sh
+++ sss.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo '<HTML><HEAD><TITLE>my_webserver</TITLE></HEAD><BODY>'; echo "<b><u> $(uptime|awk {'print $6" "$7" "$8" "$9" "$10'})</u></b><BR>"; echo "commit ID: $(git log > file;head -n 1 file | awk '{print $2}'; rm -f file;)"; echo '</BODY></HTML>';
+echo '<HTML><HEAD><TITLE>my_webserver</TITLE></HEAD><BODY>'; 
+echo "<b><u> $(uptime|awk {'print $6" "$7" "$8" "$9" "$10'})</u></b><BR>"; 
+echo "commit ID: $(cd /my_volume/my_git; git log > file;head -n 1 file | awk '{print $2}'; rm -f file;)";
+echo '</BODY></HTML>';
