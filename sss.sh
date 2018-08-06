1727333fdc1a4fc9a3ab68c32fdd67c845505b6e
dda <sralnya@gmail.com>
2018-08-06 17:44:25 +0300

    resource commit id

--- sss.sh
+++ sss.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo '<HTML><HEAD><TITLE>my_webserver</TITLE></HEAD><BODY>'; 
 echo "<b><u> $(ps -p $(ps aux | grep test.py |grep -v grep |awk {'print $2'}) -o  pcpu,pmem) </u></b><BR>"; 
+echo "commit ID: $(cd /my_volume/my_git; git log > file;head -n 1 file | awk '{print $2}'; rm -f file;)";
 echo '</BODY></HTML>';
