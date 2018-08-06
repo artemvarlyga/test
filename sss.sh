9655d3944861b34fef751f8a192fc5f183cd311e
dda <sralnya@gmail.com>
2018-08-06 18:54:00 +0300

    formetted values by string

--- sss.sh
+++ sss.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo '<HTML><HEAD><TITLE>my_webserver</TITLE></HEAD><BODY>'; 
-echo "<b><u> $(ps -p $(ps aux | grep test.py |grep -v grep |awk {'print $2'}) -o  pcpu,pmem) </u></b><BR>"; 
+echo "<b><u> $(ps -p $(ps aux | grep test.py |grep -v grep |awk {'print $2'}) -o  pcpu) </u></b><BR>"; 
+echo "<b><u> $(ps -p $(ps aux | grep test.py |grep -v grep |awk {'print $2'}) -o  pmem)<BR>"; 
 echo "commit ID: $(cd /my_volume/my_git; git log > file;head -n 1 file | awk '{print $2}'; rm -f file;)";
-echo '</BODY></HTML>';
+echo '</BODY></HTML>'
