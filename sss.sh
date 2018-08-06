fdebf4a4667ad0de3e58d9c6140cbeda86ab851b
dda <sralnya@gmail.com>
2018-08-06 16:51:39 +0300

    added memory usage

--- sss.sh
+++ sss.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo '<HTML><HEAD><TITLE>my_webserver</TITLE></HEAD><BODY>'; 
 echo "<b><u> $(uptime|awk {'print $6" "$7" "$8" "$9" "$10'})</u></b><BR>"; 
+echo "Memory usage: $(free -mh|head -n2)";
 echo "commit ID: $(cd /my_volume/my_git; git log > file;head -n 1 file | awk '{print $2}'; rm -f file;)";
 echo '</BODY></HTML>';
