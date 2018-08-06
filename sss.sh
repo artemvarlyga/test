637dcc651d929923368ed3816d389438690e22e7
dda <sralnya@gmail.com>
2018-08-06 17:38:02 +0300

    resource usage of itself

--- sss.sh
+++ sss.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo '<HTML><HEAD><TITLE>my_webserver</TITLE></HEAD><BODY>'; 
-echo "<b><u> $(uptime|awk {'print $6" "$7" "$8" "$9" "$10'})</u></b><BR>"; 
-echo "Memory usage: $(free -mh|head -n2)";
-echo "commit ID: $(cd /my_volume/my_git; git log > file;head -n 1 file | awk '{print $2}'; rm -f file;)";
+echo "<b><u> $(ps -p $(ps aux | grep test.py |grep -v grep |awk {'print $2'}) -o  pcpu,pmem) </u></b><BR>"; 
 echo '</BODY></HTML>';
